21605f5d03c4d00e632e4b8951b1f2749b75f21e
Olivier Girardot <ssaboum@gmail.com>
2014-03-16 17:17:53 +0100

    finished fabric deploy file

---
+++ fac-stemi/devops/deploy/fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import *
+
+
+def deploy(artifact):
+    TEMP_DEPLOY_PATH = '/tmp/invoice.lateralthoughts.com.zip'
+
+    # push artifact
+    put(artifact, TEMP_DEPLOY_PATH)
+
+    # stop service via supervisord
+    run('supervisorctl stop invoice_play')
+
+    # destroy old old version/save old version
+    with cd('/opt/invoice.lateral-thoughts.com/'):
+        run('tar -cf /tmp/old.invoice.lateral-thoughts.com.tar *')
+        run('rm -rf *')
+        # unzip l'artefact
+        run('unzip %s' % TEMP_DEPLOY_PATH)
+        # re-set les droits
+        run('chmod -R systemi .')
+
+    # restart service supervisord
+    run('supervisorctl start invoice_play')
+
+    # check running/status supervisord
+    # TODO
+    # reload nginx
