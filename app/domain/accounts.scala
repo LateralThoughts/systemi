14195550a0763af25555c36649d9a3aab360989b
Mac yoko <e.strauser@gmail.com>
2014-09-30 11:44:54 +0200

    add BasicProfileSerializer

--- app/domain/accounts.scala
+++ app/domain/accounts.scala
@@ -1,13 +1,18 @@
 package domain
 
-import org.bouncycastle.util.encoders.Base64
-import org.joda.time.DateTime
-import play.api.libs.json.{Json, Format, JsResult, JsValue}
-
-case class User(userId: String, firstName: String, lastName: String, email: String, avatarUrl: String)
+import play.api.libs.json.Json
+import securesocial.core._
 
 sealed trait Member
-case class Human(user: User) extends Member
+case class Human(user: BasicProfile) extends Member
 case object LT extends Member
 
 case class Account(name: String, stakeholders: List[Member] = List())
+
+trait BasicProfileSerializer {
+    implicit val passwordInfoFormatter = Json.format[PasswordInfo]
+    implicit val oAuth1InfoFormatter = Json.format[OAuth1Info]
+    implicit val oAuth2InfoFormatter = Json.format[OAuth2Info]
+    implicit val authMethodFormatter = Json.format[AuthenticationMethod]
+    implicit val basicProfileFormatter = Json.format[BasicProfile]
+}
