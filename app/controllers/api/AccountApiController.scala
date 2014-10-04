8a3f323c4a7ffb8622187edfe2df86ec792d8b4f
Vincent Doba <vincent.doba@cedeela.fr>
2014-10-04 16:13:41 +0200

    redirect to members listing after successfully creating an account, remove unused imports in AccountApiController

--- app/controllers/api/AccountApiController.scala
+++ app/controllers/api/AccountApiController.scala
@@ -1,16 +1,15 @@
 package controllers.api
 
 import domain._
-import play.api.libs.json.{JsObject, Json}
-import play.api.mvc.{Action, Controller}
+import play.api.libs.json.Json
+import play.api.mvc.Controller
 import play.modules.reactivemongo.MongoController
-import securesocial.core.{AuthenticationMethod, RuntimeEnvironment, BasicProfile}
+import securesocial.core.{RuntimeEnvironment, BasicProfile}
 import domain.Account
 import play.modules.reactivemongo.json.collection.JSONCollection
 import play.api.libs.json.JsObject
 import domain.Human
 import scala.concurrent.Future
-import play.Logger
 
 class AccountApiController(override implicit val env: RuntimeEnvironment[BasicProfile])
   extends Controller
@@ -48,7 +47,10 @@ class AccountApiController(override implicit val env: RuntimeEnvironment[BasicPr
                             name = accountName.head
                         } yield Account(name, Human(user))
                     accounts match {
-                        case Some(account) => saveAccount(account)
+                        case Some(account) => {
+                          saveAccount(account)
+                          Future(Redirect(controllers.routes.MembersController.index()))
+                        }
                         case None => Future(BadRequest)
                     }
                 case None => Future(BadRequest)
