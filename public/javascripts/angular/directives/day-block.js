f11d7cbf445464ac4b14a7a9944b893b4d46eb55
Vincent Doba <vincent.doba@cedeela.fr>
2014-12-30 20:19:31 +0100

    refactoring, remove dead code

--- public/javascripts/angular/directives/day-block.js
+++ public/javascripts/angular/directives/day-block.js
@@ -1,38 +1,7 @@
 /* Day block directive */
 
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $modal service used above.
-
-var ModalInstanceCtrl = function ($scope, $modalInstance, day) {
-  $scope.day = day;
-
-  $scope.halfUp = function() {
-  	day.halfDown = false;
-  	day.halfUp = true;
-  	$modalInstance.close();
-  }
-
-  $scope.halfDown = function() {
-  	day.halfDown = true;
-  	day.halfUp = false;
-  	$modalInstance.close();
-  }
-
-  $scope.dismiss = function () {
-  	day.halfDown = false;
-  	day.halfUp = false;
-    $modalInstance.close();
-  };
-
-  $scope.cancel = function () {
-  	day.halfDown = true;
-  	day.halfUp = true;
-    $modalInstance.dismiss('cancel');
-  };
-};
-
 angular.module("day-block", [])
-    .directive('ltDayBlock', function ($modal, $log) {
+    .directive('ltDayBlock', function () {
         return {
             templateUrl:'/assets/javascripts/angular/templates/day-block.html',
             restrict: 'EA',
@@ -40,22 +9,10 @@ angular.module("day-block", [])
             scope: {
             	day: '='
             },
-            link: function(scope, element, attrs){
+            link: function(scope){
             	scope.toggle = function(day) {
             		day.toggleNextState();
-            	}
-            	/* deprecated : open modal */
-	           	scope.open = function (day) {
-				    var modalInstance = $modal.open({
-				      templateUrl: '/assets/javascripts/angular/templates/day-block-modal.html',
-				      controller: ModalInstanceCtrl,
-				      resolve: {
-				        day: function () {
-				          return scope.day;
-				        }
-				      }
-				    });
-				};
+            	};
             }
         }
     });
