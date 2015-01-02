1619be123bac15db8d341744f42eb37384b108be
Vincent Doba <vincent.doba@cedeela.fr>
2015-01-02 23:37:39 +0100

    complete karma test

--- test/angular-test/unit-test/invoice.js
+++ test/angular-test/unit-test/invoice.js
@@ -13,29 +13,28 @@ describe("Fac stemi - invoice", function () {
             $httpBackend = $injector.get('$httpBackend');
             scope = rootScope.$new();
 
-
             createInvoiceCreateCtrl = function() {
                 return controller('CreateCtrl', {
                     $scope: scope
                 });
             };
-
-
-
-
         })
     });
 
-	it('should initialize one task line in scope', function () {
+	it('should initialize invoice model in scope', function () {
 
-        $httpBackend.whenGET("/api/invoice/numbers/last").respond([{
+        $httpBackend.whenGET("/api/invoice/numbers/last").respond({
             prefix: "VT",
             value: 150
-        }]);
+        });
 
 
         createInvoiceCreateCtrl();
         $httpBackend.flush();
+
+        expect(scope.invoice.invoiceNumber).toBe("VT150");
+        expect(scope.invoice.paymentDelay).toBe(30);
+        expect(scope.invoice.withTaxes).toBe(true);
 
         expect(scope.invoice.invoice.length).toBe(1);
         expect('addButtonVisible' in scope.invoice.invoice[0]).toBe(true);
